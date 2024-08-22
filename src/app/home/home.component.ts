02cb7b8412f59e47849de283058364e111964161
Akshay Kumar <akshaylalti@outlook.com>
2024-08-22 17:52:17 +0530

    home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,21 +9,9 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+
   ngOnInit(): void {
-  }
-  ngAfterViewInit(): void {
-    $('.header-carousel').owlCarousel({
-      autoplay: true,
-      smartSpeed: 1500,
-      items: 1,
-      dots: true,
-      loop: true,
-      nav: true,
-      navText: [
-        '<i class="bi bi-chevron-left"></i>',
-        '<i class="bi bi-chevron-right"></i>'
-      ]
-    });
+
   }
 
 }
